ac045c7b23f1155cb23f5599cd290f5612b4d275
jigar <jigar.joshi011@gmail.com>
2024-07-08 13:24:25 +0530

    init

---
+++ hooks/useScreenDetector.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export const useScreenDetector = (): {
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+} => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+  const isTablet = width <= 1024;
+  const isDesktop = width > 1024;
+
+  return { isMobile, isTablet, isDesktop };
+};
