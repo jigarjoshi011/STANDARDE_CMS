db01bb426fdbb45022110dffa33326d5e6ded28d
Jigar <jigar.joshi011@gmail.com>
2024-07-08 23:01:37 +0530

    working on UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header/Header";
-
-
+import Footer from "@/components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >
+      <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
